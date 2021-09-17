c768cacd43b2e15dd999fc2b38ae7473ef002ad4
mibrahim234 <mohamm1417@gmail.com>
2021-09-17 16:17:42 -0400

    fixed btn col

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-fetch()
+var cityInputEl = document.querySelector("#city-entry");
+var searchEl = document.querySelector("#search");
+var weatherEl = document.querySelector("#weatherboxcontainer");
+var ForecastEl = document.querySelector("#5dayforecast");
+
+
+

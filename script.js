43a4fd96677933c5365206bc202bcd4736d354db
mibrahim234 <mohamm1417@gmail.com>
2021-09-17 19:29:44 -0400

    added functions

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 var cityInputEl = document.querySelector("#city-entry");
-var searchEl = document.querySelector("#search");
+var searchBtn = document.querySelector("#search");
 var weatherEl = document.querySelector("#weatherboxcontainer");
-var ForecastEl = document.querySelector("#5dayforecast");
+var forecastEl = document.querySelector("#fivedayforecast");
+var cityName = "";
+var cityNameH1 = document.querySelector("#blueberry");
 
+function getCityInfo (event) {
+event.preventDefault();
+var userInput = cityInputEl.value;
+cityName = userInput.trim().toLowerCase();
+getCurrentWeather();
+}
 
+function getCurrentWeather () {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=ec104f2f80e34aa355099c98f2584fe7`)
+    .then(response => response.json())
+    .then(data => renderCurrentWeather(data));
+}
 
+function renderCurrentWeather(data) {
+console.log(data);
+ //cityNameH1.textContent = data.city.name;
+ var h1 = document.createElement("h1");
+ h1.textContent = data.city.name;
+ weatherEl.appendChild(h1);
+}
+  searchBtn.addEventListener("click", getCityInfo)
+
+  
+  
+  
